f02b19fcbb5bc223532dc08f6a6aeba767c87c07
Álvaro <alvaro.d.jimenez@gmail.com>
2016-01-20 14:13:39 +0100

    Repositorio con la dll que implementa el adaptador.

---
+++ RepositorioAdapter/Adapter/BaseAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RepositorioAdapter.Adapter
+{
+    public abstract class BaseAdapter<TEntity, TModel> : IAdapter<TEntity, TModel>
+    {
+        public ICollection<TModel> FromModel(ICollection<TEntity> model)
+        {
+            return model.Select(FromModel).ToList();
+        }
+
+        public abstract TModel FromModel(TEntity model);
+
+        public ICollection<TEntity> FromViewModel(ICollection<TModel> model)
+        {
+            return model.Select(FromViewModel).ToList();
+        }
+
+        public abstract TEntity FromViewModel(TModel model);
+    }
+}
---
+++ RepositorioAdapter/Adapter/IAdapter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace RepositorioAdapter.Adapter
+{
+    /// <summary>
+    /// Interfaz que deberá implementar el adaptador/parseador.
+    /// </summary>
+    /// <typeparam name="TEntity">Objeto de entidad en la base da datos</typeparam>
+    /// <typeparam name="TModel">Objeto de transferencia, es lo que mando al móvil</typeparam>
+    public interface IAdapter <TEntity, TModel>
+    {
+        TEntity FromViewModel(TModel model);
+        TModel FromModel(TEntity model);
+
+        ICollection<TEntity> FromViewModel(ICollection<TModel> model);
+        ICollection<TModel> FromModel(ICollection<TEntity> model);
+
+    }
+}
